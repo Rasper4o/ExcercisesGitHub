0c1fc69a1675b63d31894db1ac91afcd01f7fd58
Kalin Dimitrov <Kalin Dimitrov>
2016-09-22 20:24:38 +0300

    Added Comments..

--- RadianstoDegrees/RadiansToDegrees.cs
+++ RadianstoDegrees/RadiansToDegrees.cs
@@ -10,11 +10,12 @@ namespace RadianstoDegrees
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("This program turns radians to degrees!");
+            //User input
             double rad = double.Parse(Console.ReadLine());
-
+            //transform
             double deg = rad * 150 / Math.PI;
-
+            //Output to screen
             Console.WriteLine(Math.Round(deg, 0));
         }
     }
